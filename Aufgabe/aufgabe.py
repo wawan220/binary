b43e5ef79e4c17aa6f6304df7ea2d16fcff62c81
wawan220 <mail@pohl-waldemar.info>
2025-11-21 13:19:17 +0100

    creadte

---
+++ Aufgabe/aufgabe.py
@@ -0,0 +1,35 @@
+"""
+teil 1:
+Erstelle eine Funktion, das zu einer Zahl in Binärdarstellung die entsprechende Zahl in
+der Dezimaldarstellung ausgibt.
+Bsp.:
+Eingabe: 101100
+Ausgabe: 44
+"""
+
+"""
+teil 2
+Erstelle ein Funktion, das zu einer Zahl in Dezimaldarstellung die entsprechende
+Zahl in der Binärdarstellung ausgibt.
+Bsp.:
+Eingabe: 44
+Ausgabe: 101100
+"""
+
+"""
+teil 3
+lagere die beiden Funktionenn aus und rufe sie über main.py auf
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
